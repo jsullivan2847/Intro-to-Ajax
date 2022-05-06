f6f5566af7139670ffd243b31ce98ec7968eb040
James Sullivan <jsullivan2847@gmail.com>
2022-05-06 10:48:15 -0400

    added functionality for displaying movie posters, and cleaned up code a bit. Functionality for user input search has begun but needs work

--- script.js
+++ script.js
@@ -5,37 +5,56 @@
 //1.jQuery $.ajax() function
 //2.The Browser's fetch{} function
 //3.Axios....http client library
+
+//variables
 const $title = $("#title");
 const $year = $("#year");
 const $rated = $("#rated");
+const $poster = $('#poster');
 const $input = $('input[type="text"]');
+const url = "http://www.omdbapi.com/?apikey=1d3e34e8&t=Die+Hard";
 
-$("form").on("submit", HandleGetData);
+//I do not understand this variable lol
+let  movieData, userInput;
 
-const url = "http://www.omdbapi.com/?apikey=1d3e34e8&";
+
+//Targets the input / submit buttons ("form")
+//on click of submit button, run the HandleGetData function
+ $("form").on("submit", HandleGetData, function(){
+  console.log("works")
+});
+
+
+
+
+//FUNCTIONS
 
 function HandleGetData(event) {
   //prevents page from being refreshed
-  event.preventDefault();
+  //event.preventDefault();
   //gets user input from input html element
   userInput = $input.val();
 
-  $.ajax(url).then(
-    function (data) {
-      data = movieData;
-      render(data);
+  $.ajax(url + userInput).then(function (data) {
+    console.log('movie data ready');
+    movieData = data;
+    render();
     },
     function (error) {
-      console.log("bad request: ", error);
-      console.log(error);
-    }
-  );
+      console.log("bad request: ");
+      //console.log(error);
+    })
 }
 
+//display data called once data is retrieved in
+// handle data
 function render() {
   $title.text(movieData.Title);
   $year.text(movieData.Year);
   $rated.text(movieData.Rated);
+  $('main').append(`<img src="${movieData.Poster}"/>`);
+  //$poster.(movieData.Poster);
 }
 
-//console.log($.ajax(url))
+HandleGetData();
+
